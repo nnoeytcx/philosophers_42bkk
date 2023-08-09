5aa4f09ef1cc8bc00518624d851e9effe0c003d7
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2023-08-09 21:17:05 +0700

    philo

---
+++ init.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 15:59:34 by tpoungla          #+#    #+#             */
+/*   Updated: 2023/08/06 22:31:09 by tpoungla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_initrule(t_rule *rule, char **tab)
+{
+	rule->no_of_philo = ft_atoi(tab[0]);
+	rule->time_to_die = ft_atoi(tab[1]);
+	rule->time_to_eat = ft_atoi(tab[2]);
+	rule->time_to_sleep = ft_atoi(tab[3]);
+	rule->state = 1;
+	if (tab[4])
+		rule->eat_times = ft_atoi(tab[4]);
+	else
+		rule->eat_times = -1;
+}
+
+t_philo	*ft_initphilo(t_rule *rule)
+{
+	int		i;
+	t_philo	*philo;
+
+	philo = malloc(sizeof(t_philo) * rule->no_of_philo);
+	if (!philo)
+		return (NULL);
+	i = 0;
+	while (i < rule->no_of_philo)
+	{
+		philo[i].id = i + 1;
+		philo[i].right = i;
+		philo[i].left = (i + 1) % rule->no_of_philo;
+		philo[i].eaten = 0;
+		philo[i].alive = 1;
+		i++;
+	}
+	return (philo);
+}
+
+pthread_mutex_t	*ft_initfork(int no_of_philo)
+{
+	int				i;
+	pthread_mutex_t	*fork;
+
+	fork = NULL;
+	fork = malloc(sizeof(pthread_mutex_t) * no_of_philo);
+	if (!fork)
+		return (NULL);
+	i = 0;
+	while (i < no_of_philo)
+	{
+		pthread_mutex_init(&fork[i], NULL);
+		i++;
+	}
+	return (fork);
+}
+
+int	ft_init(t_main *main, char **tab)
+{
+	ft_initrule(&main->rule, tab);
+	if (main->rule.no_of_philo == 0 || main->rule.eat_times == 0)
+		return (0);
+	main->philo = ft_initphilo(&main->rule);
+	if (!main->philo)
+		return (0);
+	main->fork = ft_initfork(main->rule.no_of_philo);
+	if (!main->fork)
+		return (0);
+	free_tab(tab);
+	return (1);
+}
---
+++ pthread.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pthread.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 16:44:02 by tpoungla          #+#    #+#             */
+/*   Updated: 2023/08/06 22:32:01 by tpoungla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	sleep_think(t_philo *philo, t_main *main)
+{
+	printf(SLEEP, CYAN, get_ms() - main->rule.start_time, philo->id);
+	time_to_action(main->rule.time_to_sleep);
+	printf(THK, GREEN, get_ms() - main->rule.start_time, philo->id);
+}
+
+void	philo_eat(t_philo *philo, pthread_mutex_t *fork, t_main *main)
+{
+	pthread_mutex_lock(&fork[philo->right]);
+	printf(FRK, MAGENTA, get_ms() - main->rule.start_time, philo->id);
+	pthread_mutex_lock(&fork[philo->left]);
+	printf(FRK, MAGENTA, get_ms() - main->rule.start_time, philo->id);
+	printf(EAT, YELLOW, get_ms() - main->rule.start_time, philo->id);
+	philo->eaten++;
+	philo->stmeal = get_ms();
+	time_to_action(main->rule.time_to_eat);
+	pthread_mutex_unlock(&fork[philo->right]);
+	pthread_mutex_unlock(&fork[philo->left]);
+}
+
+void	*routine(void *arg)
+{
+	t_main	*main;
+	int		i;
+
+	main = (t_main *)arg;
+	i = main->no_philo;
+	main->philo[i].stmeal = main->rule.start_time;
+	while (main->rule.state)
+	{
+		philo_eat(&main->philo[i], main->fork, main);
+		sleep_think(&main->philo[i], main);
+	}
+	return (NULL);
+}
+
+int	ft_strtheard(t_main *main)
+{
+	int	i;
+
+	i = 0;
+	main->rule.start_time = get_ms();
+	while (i < main->rule.no_of_philo)
+	{
+		main->no_philo = i;
+		if (pthread_create(&main->philo[i].th, NULL, &routine, main))
+			return (0);
+		if (pthread_detach(main->philo[i].th))
+			return (0);
+		usleep(10);
+		i += 2;
+		if (i >= main->rule.no_of_philo && i % 2 == 0)
+		{
+			usleep (50);
+			i = 1;
+		}
+	}
+	return (1);
+}
